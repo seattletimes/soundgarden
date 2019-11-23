4934a3b25fb8caf56728e14d89f774ca9a4391a6
Miranda Hardy <mhardy@seattletimes.com>
2019-11-22 16:15:43 -0800

    Cluster of bubbles with labels and conditional color

--- src/js/main.js
+++ src/js/main.js
@@ -6,137 +6,101 @@ var d3 = require("d3");
 var bandData = require("./graph.json");
 
 var width = 960,
-    height = 500;
+    height = 900;
 
+var colors = ["#e41a1c", "#377eb8", "#4daf4a", "#984ea3", "#ff7f00", "yellow"];
 
-function makeDisplay(data){
-
-  var margin = {top: 20, right: 20, bottom: 20, left: 20};
-  var numBins=3;
-  var binWidth =width / numBins - (margin.right+margin.left);
-  var binHeight = height-margin.top-margin.bottom;
-  var binStart= (binWidth + margin.right + margin.left);
-  var radius = 20;
-
-  var force = d3.forceSimulation(bandData.nodes)
-    .force("charge", d3.forceManyBody().strength(-200))
+var force = d3.forceSimulation(bandData.nodes)
+    .force("charge", d3.forceManyBody().strength(-20))
     .force("link", d3.forceLink(bandData.edges))
-    .on("tick", tick); 
+    .force("center", d3.forceCenter(width / 2, height / 2))
+    .on("tick", tick) ;
 
-  var svg = d3.select("body").append("svg")
+var svg = d3.select("body").append("svg")
     .attr("width", width)
-    .attr("height", height)
-    .append("g")
-    .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+    .attr("height", height);
 
-  var bins = svg.selectAll("g")
-              .data([{label:"Soundgarden"},{label:"Members"},{label:"Projects"}])
-              .enter().append("g")
-              .attr("class", "iRect")
-              .attr("transform", function(d, i) { return "translate(" + binStart * i + ")"; });  
+var defs = svg.append("defs");
 
-  bins.append("rect")
-      .attr("id", function(d, i) { return "bin"+i;})
-      .attr("width", binWidth)
-      .attr("height", binHeight)
-      .style("fill", "yellow");
+var gradient = defs.append("linearGradient")
+    .attr("id", "svgGradient")
+    .attr("x1", "40%")
+    .attr("x2", "100%")
+    .attr("y1", "0%")
+    .attr("y2", "100%");
 
-  svg.selectAll("#bin0")
-    .style("fill", "red");
-  
-  var link = svg.selectAll("line")
-    .data(data.edges)
-    .enter()
-    .append("line")
-    .attr("class","edge")
-    .style("stroke", "#ccc")
-    .style("stroke-width", 2);
+gradient.append("stop")
+    .attr('class', 'start')
+    .attr("offset", "0%")
+    .attr("stop-color", "orange")
+    .attr("stop-opacity", 1);
 
+gradient.append("stop")
+    .attr('class', 'end')
+    .attr("offset", "100%")
+    .attr("stop-color", "blue")
+    .attr("stop-opacity", 1);
 
-  var node = svg.selectAll("g")
-    .data(data.nodes)
-    .enter()
-    .append("g")
-    .attr("cx", function(d) { return d.cx; })
-    .attr("cy", function(d) { return d.cy; })
-    .attr("width", 40)
-    .attr("height", 40)
-    .on("click", click);
+var link = svg.append("g")
+    .attr("class", "links")
+    .selectAll("line")
+    .data(bandData.edges)
+    .enter().append("line")
+    .attr("stroke-width", 2);
 
-  node
-    .append("circle")
-    .attr("r", radius)
-    .style("fill", "blue")
-    .style("stroke", "#ccc");
+var node = svg.append("g")
+    .attr("class", "nodes")
+    .selectAll("g")
+    .data(bandData.nodes)
+    .enter().append("g")
+    .attr("class", function(d){
+        return d.group;
+    })
+    
+    
+var circles = node.append("circle")
+    .attr("r", function(d){
+        if(d.group == 3){ return 80; }
+        else if (d.group == 2) { return 60; }
+        else return 30;
+    } );
+    // .attr("border", "10px solid")
+    // .attr("border-image", "conic-gradient(red, yellow, lime, aqua, blue, magenta, red) 1");
+    
+    
+circles.attr("fill", function(d){
+    if(d.member<5) return colors[d.member];
+    else return "url(#svgGradient)";
+});
 
-  node
-    .append("text")
-    .text(function(d){
-        return d.name;
+var lables = node.append("text")
+    .text(function(d) {
+      return d.name;
     })
     .attr("text-anchor", "middle");
 
+node.append("title")
+    .text(function(d) { return d.name; });
+
+
+
+
+
+
+function tick() {
+  link
+      .attr("x1", function(d) { return d.source.x; })
+      .attr("y1", function(d) { return d.source.y; })
+      .attr("x2", function(d) { return d.target.x; })
+      .attr("y2", function(d) { return d.target.y; });
+
   node
-    .append("title")
-        .text(function(d){
-            return d.name;
-    });
-
-// var node = svg.selectAll(".node").data(data.nodes);
-
-// var nodeEnter = svg.selectAll("g").data(data.nodes).enter().append("g")
-//     .attr("class", "node")
-//     .on("click", click);
-
-// nodeEnter.append("circle")
-//     .attr("r", 20)
-
-// nodeEnter.append("text")
-//     .attr("dy", ".35em")
-//     .text(function(d){ return d.name });
-
-// node.select("circle")
-//     .style("fill", "#f46f08");
-
-
-
-
-
-    function click(){
-        console.log("clicked " + this);
-    }
-    // ADD LATER style edges on hover
-    // node.on('mouseover', function(d) {
-    // link.attr('class', function(l) {
-    //     if (d === l.source || d === l.target)
-    //     return "link active";
-    //     else
-    //     return "link inactive";
-    // });
-    // });
-
-    // // Set the stroke width back to normal when mouse leaves the node.
-    // node.on('mouseout', function() {
-    //     link.attr('class', "link");
-    //     })
-    //     .on('click', click);
-
-
-
-  function tick(e) {
-    link.attr("x1", function(d) { return d.source.x; })
-            .attr("y1", function(d) { return d.source.y; })
-            .attr("x2", function(d) { return d.target.x; })
-            .attr("y2", function(d) { return d.target.y; });
-    // Push different nodes in different directions for clustering.
-    // the Math.max bits will keep the nodes confined in there appropriate "bins"
-    // fyi: binStart * (d.group-1) → left border
-    // and: binStart * (d.group-1) + binWidth → right border
-    node
-        .attr("cx", (function(d) 
-            { d.x = Math.max(binStart * (d.group-1), Math.min((binStart * (d.group-1) + binWidth) - radius, d.x)); return d.x; }))
-        .attr("cy", (function(d) {return d.y = Math.max(radius, Math.min(binHeight - radius, d.y));}));
-  }
+      .attr("transform", function(d) {
+        return "translate(" + d.x + "," + d.y + ")";
+      })
 }
 
-makeDisplay(bandData);
+
+
+
+
